acd039452802c523da1c17f0ee75f28224e5b57e
Scott Garcia <Scott.Garcia@capitalone.com>
2022-01-14 00:28:46 -0500

    metro hero fix, off hours feature

--- src/config.py
+++ src/config.py
@@ -6,41 +6,72 @@ config = {
 	#########################
 
 	# WIFI Network SSID
-	'wifi_ssid': '<Your 2.4ghz WiFi SSID>',
+	'wifi_ssid': '',
 
 	# WIFI Password
-	'wifi_password': '<Your WiFi Password>',
+	'wifi_password': '',
 
 	#########################
 	# Metro Configuration   #
 	#########################
+	# WMATA API KEY
+	'source_api': 'WMATA', # WMATA or MetroHero.
+	'metro_api_key1': '',
+	'metro_api_key2': '',
 
 	# Metro Station Code
-	'metro_station_code': 'D02',
+	#Cap South, Navy Yard
+	'metro_station_code': ['E03','C02'],
 
 	# Metro Train Group
-	'train_group': '2',
+	'train_group': ['2','2'],
 
-	# API Key for WMATA
-	'metro_api_key': '<Your WMATA API Key>',
+	#Walking Distance Times, ignore trains arriving in less than this time
+	# [2, 12]
+	'walking_time': [8, 8],
 
 	#########################
 	# Other Values You      #
 	# Probably Shouldn't    #
 	# Touch                 #
 	#########################
-	'metro_api_url': 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/',
-	'metro_api_retries': 2,
+	# WMATA API
+	'metro_api_url1': 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/',
+
+	# # MetroHero API - Not Current Working with the code. No need to fill or use
+	'metro_api_url2': 'https://dcmetrohero.com/api/v1/metrorail/stations/[stationCode]/trains?includeScheduledPredictions=True',
+
+	'metro_api_retries': 3,
 	'refresh_interval': 5, # 5 seconds is a good middle ground for updates, as the processor takes its sweet ol time
 
-	# Display Settings
+        # Full names mapped to abbreviations for MetroHero
+        'station_mapping': {
+            'Branch Avenue': 'Brnch Av',
+            'Huntington': 'Hntingtn',
+            'Vienna/Fairfax-GMU': 'Vienna',
+            'Franconia-Springfield': 'Frnconia',
+            'New Carrollton': 'NewCrltn',
+            'Greenbelt': 'Grnbelt',
+            'Huntington': 'Hntingtn',
+            'Largo Town Center': 'Largo',
+            'Twinbrook': 'Twinbrk',
+            'Wiehle-Reston East': 'Wiehle',
+            'No Passenger': 'No Psngr',
+            'NoPssenger': 'No Psngr',
+            'ssenger': 'No Psngr'
+
+        },
+
+        #########################
+        # Display Configuration #
+        #########################
 	'matrix_width': 64,
 	'num_trains': 3,
 	'font': bitmap_font.load_font('lib/5x7.bdf'),
 
-	'character_width': 5,
-	'character_height': 7,
-	'text_padding': 1,
+	'character_width': 5, #5
+	'character_height': 6, #7
+	'text_padding': 2, #1
 	'text_color': 0xFF7500,
 
 	'loading_destination_text': 'Loading',
@@ -50,9 +81,24 @@ config = {
 	'heading_text': 'LN DEST   MIN',
 	'heading_color': 0xFF0000,
 
-	'train_line_height': 6,
-	'train_line_width': 2,
+	'train_line_height': 6, #6
+	'train_line_width': 4,
 
 	'min_label_characters': 3,
 	'destination_max_characters': 8,
+
+
+        #############################
+        # Off Hours Configuration   #
+        #############################
+        # adafruit io settings, necessary for determining current time to sleep
+        # An account is free to set up, instructions below
+        # https://learn.adafruit.com/adafruit-magtag/getting-the-date-time
+        'aio_username': '',
+        'aio_key': '',
+        'timezone': 'America/New_York', # http://worldtimeapi.org/timezones
+
+        # Time of day to turn board on and off - must be 24 hour "HH:MM"
+        'display_on_time': "07:00",
+        'display_off_time': "22:00",
 }
--- src/metro_api.py
+++ src/metro_api.py
@@ -1,56 +1,100 @@
 import board
-from adafruit_matrixportal.network import Network
+import time
 
 from config import config
 from secrets import secrets
-
-# Keeping a global reference for this
-_network = Network(status_neopixel=board.NEOPIXEL)
 
 class MetroApiOnFireException(Exception):
     pass
 
 class MetroApi:
-    def fetch_train_predictions(station_code: str, group: str) -> [dict]:
-        return MetroApi._fetch_train_predictions(station_code, group, retry_attempt=0)
+    def __init__(self):
+        pass
 
-    def _fetch_train_predictions(station_code: str, group: str, retry_attempt: int) -> [dict]:
+    def fetch_train_predictions(self, wifi, station_codes, groups, walks={}) -> [dict]:
+        return self._fetch_train_predictions(wifi, station_codes, groups, walks, retry_attempt=0)
+
+    def _fetch_train_predictions(self, wifi, station_codes, groups, walks, retry_attempt: int) -> [dict]:
         try:
-            api_url = config['metro_api_url'] + station_code
-            train_data = _network.fetch(api_url, headers={
-                'api_key': config['metro_api_key']
-            }).json()
+            print('Fetching...')
+            start = time.time()
 
-            print('Received response from WMATA api...')
+            if config['source_api'] == 'WMATA':
+                # WMATA Method
+                api_url = config['metro_api_url1'] + ','.join(set(station_codes))
+                response = wifi.get(api_url, headers={'api_key': config['metro_api_key1']}, timeout=1).json()
+                trains = list(filter(lambda t: (t['LocationCode'], t['Group']) in groups, response['Trains']))
+            else:
+                #Metro Hero Method
+                trains = []
+                for station in set(station_codes): # select trains in desired direction
+                    api_url = config['metro_api_url2'].replace('[stationCode]', station)
+                    response = wifi.get(api_url, headers={'apiKey': config['metro_api_key2']}, timeout = 30).json()
+                    # MetroHero returns a lot of results, truncate to save memory
+                    if len(response) > 5:
+                        response = response[:5]
+                    trains.extend(list(filter(lambda t: (station, t['Group']) in groups, response)))
 
-            trains = filter(lambda t: t['Group'] == group, train_data['Trains'])
+            print('Received response from ' + config['source_api'] + ' api...')
+            TIME_BUFFER = round((time.time() - start)/60) + 1
+            trains = [self._normalize_train_response(t, TIME_BUFFER) for t in trains]
 
-            normalized_results = list(map(MetroApi._normalize_train_response, trains))
+            if walks != {}:
+                trains = list(filter(lambda t: self.arrival_map(t['arrival'])-walks[t['loc']] >= 0, trains))
 
-            return normalized_results
-        except RuntimeError:
+            if len(groups) > 1:
+                trains = sorted(trains, key=lambda t: self.arrival_map(t['arrival']))
+
+            print(trains)
+            print('Time to Update: ' + str(time.time() - start))
+            return trains
+
+        except Exception as e:
+            print(e)
             if retry_attempt < config['metro_api_retries']:
-                print('Failed to connect to WMATA API. Reattempting...')
+                print('Failed to connect to API. Reattempting...')
                 # Recursion for retry logic because I don't care about your stack
-                return MetroApi._fetch_train_predictions(station_code, group, retry_attempt + 1)
+                return self._fetch_train_predictions(wifi, station_codes, groups, walks, retry_attempt + 1)
             else:
                 raise MetroApiOnFireException()
-    
-    def _normalize_train_response(train: dict) -> dict:
+
+    def arrival_map(self, arr):
+        if arr == 'BRD':
+            return 0
+        elif arr == 'ARR':
+            return 1
+        elif not arr.isdigit():
+            return 100
+        else:
+            return int(arr)
+
+    def _normalize_train_response(self, train: dict, buff:int) -> dict:
         line = train['Line']
         destination = train['Destination']
-        arrival = train['Min']
+        loc = train['LocationCode']
 
-        if destination == 'No Passenger' or destination == 'NoPssenger' or destination == 'ssenger':
-            destination = 'No Psngr'
+        if 'minutesAway' in train:
+            arrival = round(float(train['minutesAway']))
+        else:
+            arrival = int(train["Min"])
+
+        arrival = arrival - buff
+        if arrival <= 0:
+            arrival = 'ARR'
+        else:
+            arrival = str(arrival)
+
+        if destination in config["station_mapping"]:
+            destination = config["station_mapping"][destination]
 
         return {
-            'line_color': MetroApi._get_line_color(line),
-            'destination': destination,
-            'arrival': arrival
+            'line_color': self._get_line_color(line),
+            'destination': destination[:config['destination_max_characters']],
+            'arrival': arrival,
+            'loc': loc
         }
-    
-    def _get_line_color(line: str) -> int:
+
+    def _get_line_color(self, line: str) -> int:
         if line == 'RD':
             return 0xFF0000
         elif line == 'OR':
